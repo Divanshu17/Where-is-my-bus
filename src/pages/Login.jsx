41ce35ee3aa18dd2dcd17593107bda83353e0037
Divanshu17 <divansukachhawacsc@gmail.com>
2025-02-24 09:38:52 +0530

    added login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -52,6 +52,15 @@ function Login() {
               name="email"
               value={formData.email}
               onChange={handleChange}
+
+
+
+
+
+
+
+
+              
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
